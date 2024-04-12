ce74fd9e342ea709876754d106d2f87dc0113648
Matthew Nock <46355885+helloagonial@users.noreply.github.com>
2024-04-12 09:09:41 +0100

    Bullet lifetime
    added a timer to bullet.cs so the bullets are destroyed if they don't hit anything after 30 seconds

--- Space/bullet.cs
+++ Space/bullet.cs
@@ -3,23 +3,41 @@ using System;
 
 public partial class bullet : RigidBody3D
 {
-	// Called when the node enters the scene tree for the first time.
+	private Timer timer;
+	private void OnTimerTimeout()
+	{
+		GD.Print("bullet killed");
+		QueueFree();
+	}
 
+	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		timer = new Timer
+		{
+			WaitTime = 30.0f,
+			OneShot = true,
+			Autostart = true
+		};
+		AddChild(timer);
+		Callable callable = new Callable(this, nameof(OnTimerTimeout));
+		timer.Connect("timeout", callable);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 	}
-	private void areaEnter(Node3D body){
-	if(body != this){
+
+	private void areaEnter(Node3D body)
+	{
+		if(body != this)
+		{
 			GD.Print("hit");
 			GD.Print(body);
 			QueueFree();
+		}
 	}
 
 
-	}
 }
